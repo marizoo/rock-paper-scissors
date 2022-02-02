55804ecdd299adb2f20a85942c9708af12d6ddca
marizoo <marizoo@meow.woof>
2022-02-02 20:02:11 +0700

    version1-logic's : 100%

---
+++ app.js
@@ -0,0 +1,68 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
+// make a global variable
+let userChoice;
+let computerChoice;
+let result;
+
+// listen for clicks
+possibleChoices.forEach(
+  (possibleChoice) => possibleChoice,
+  addEventListener("click", (ev) => {
+    userChoice = ev.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+  })
+);
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "paper";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "scissors";
+  }
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "it's a draw!";
+  }
+
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lose!";
+  }
+
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose!";
+  }
+
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!";
+  }
+
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  }
+
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose!";
+  }
+
+  resultDisplay.innerHTML = result;
+}
